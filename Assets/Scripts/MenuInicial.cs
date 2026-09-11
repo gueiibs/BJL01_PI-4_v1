85fdfcec3c55ee51377093f150397f83d5b05ff7
Gabriela Serras <gabrielaserras15@gmail.com>
2026-09-11 19:53:42 -0300

    first commit
    criando primeira etapa do menu inicial

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuInicial : MonoBehaviour
+{
+    [Header("Paineis")]
+    [SerializeField] GameObject painelMenu;
+    [SerializeField] GameObject painelCreditos;
+    [SerializeField] GameObject painelConfiguracoes;
+
+
+    public void BotaoJogar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scene01");
+    }
+    public void BotaoCreditos()
+    {
+        FecharTodosPaineis();
+        painelCreditos.SetActive(true);
+    }
+    public void BotaoConfiguracoes()
+    {
+        FecharTodosPaineis();
+        painelConfiguracoes.SetActive(true);
+    }
+    public void BotaoVoltarMenu()
+    {
+        FecharTodosPaineis();
+        painelMenu.SetActive(true);
+    }
+    public void BotaoSair()
+    {
+        Application.Quit();
+    }
+
+    void FecharTodosPaineis()
+    {
+        painelMenu.SetActive(false);
+        painelCreditos.SetActive(false);
+        painelConfiguracoes.SetActive(false);
+    }
+}
